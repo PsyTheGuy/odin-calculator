f68138d76267078101c7f206005ed6bf4016115b
PsyTheGuy <psyplaysgames@gmail.com>
2022-08-17 14:17:15 +0200

    create operate function

---
+++ script.js
@@ -0,0 +1,14 @@
+function operate(num1, num2, operand) {
+  switch (operand) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+    default:
+      break;
+  };
+}
